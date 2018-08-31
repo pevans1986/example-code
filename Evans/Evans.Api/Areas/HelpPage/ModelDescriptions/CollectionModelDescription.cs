a66baec2d4f58c59d7669a3a0b45fd253e43bd58
Paul Evans <pevans1986@gmail.com>
2018-08-30 23:42:41 -0400

    Removed default help documentation and related content.

--- Evans/Evans.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Evans.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
