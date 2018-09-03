0310cd47112d040d07cd2a288b67d050516f5bbb
Paul Evans <pevans1986@gmail.com>
2018-09-03 16:37:51 -0400

    Added api controller for email addresses.

---
+++ Evans/Evans.Api/Controllers/EmailAddressesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Evans.Core.Api;
+using Evans.Core.Service;
+using Evans.Models;
+
+namespace Evans.Api.Controllers
+{
+	public class EmailAddressesController : WebApiController<EmailAddress>
+	{
+		#region Public Constructors
+
+		public EmailAddressesController(IService<EmailAddress> service) : base(service)
+		{
+		}
+
+		#endregion Public Constructors
+	}
+}
---
+++ Evans/Evans.Core/Repository/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Evans.Core.Models;
+
+namespace Evans.Core.Repository
+{
+	public abstract class Repository<TModel>
+		where TModel : class, IDomainEntity
+	{
+
+	}
+}
