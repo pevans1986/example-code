d654a6a299448e1faf1f2f7a7b0b3e92fb6ec519
Paul Evans <pevans1986@gmail.com>
2020-11-11 17:22:15 -0500

    Added unit tests for MemberInfoExtensions

--- Evans/Evans.Core.Extensions.Tests/System/Reflection/MemberInfoExtensionsTests.cs
+++ Evans/Evans.Core.Extensions.Tests/System/Reflection/MemberInfoExtensionsTests.cs
@@ -1,12 +1,11 @@
-﻿using NUnit.Framework;
-using Evans.Core.Extensions.System.Reflection;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
+
+using NUnit.Framework;
 
 namespace Evans.Core.Extensions.System.Reflection.Tests
 {
@@ -27,34 +26,55 @@ namespace Evans.Core.Extensions.System.Reflection.Tests
 			Assert.IsNotNull(memberInfo2.GetAttribute<Attribute1>(true));
 		}
 
-		//[Test]
-		//public void GetAttributeOrDefaultTest()
-		//{
-		//	Assert.Fail();
-		//}
+		[Test]
+		public void GetAttributeOrDefaultTest()
+		{
+			var memberInfo = typeof(AttributeTestSubClass);
+			var attribute = memberInfo.GetAttributeOrDefault<Attribute1>();
 
-		//[Test]
-		//public void GetAttributesTest()
-		//{
-		//	Assert.Fail();
-		//}
+			Assert.IsNotNull(attribute);
 
-		//[Test]
-		//public void HasAttributeTest()
-		//{
-		//	Assert.Fail();
-		//}
+			var attribute2 = memberInfo.GetAttributeOrDefault<Attribute2>();
+			Assert.IsNotNull(attribute2);
+		}
 
-		//[Test]
-		//public void HasAttributeTest1()
-		//{
-		//	Assert.Fail();
-		//}
+		[Test]
+		public void GetAttributesTest()
+		{
+			var attributes = typeof(AttributeTestClass).GetAttributes<AttributeMulti>();
+			int attributesCount = attributes.Count();
+			Assert.AreEqual(1, attributesCount);
+
+			var attributes2 = typeof(AttributeTestSubClass).GetAttributes<AttributeMulti>(inherit: false);
+			int attributesCount2 = attributes2.Count();
+			Assert.AreEqual(2, attributesCount2);
+
+			var attributes3 = typeof(AttributeTestSubClass).GetAttributes<AttributeMulti>(inherit: true);
+			Assert.AreEqual(attributesCount + attributesCount2, attributes3.Count());
+		}
+
+		[Test]
+		public void HasAttribute_T_Test()
+		{
+			var memberInfo = typeof(AttributeTestSubClass);
+			Assert.IsTrue(memberInfo.HasAttribute<Attribute2>());
+			Assert.IsFalse(memberInfo.HasAttribute<Attribute1>(inherit: false));
+			Assert.IsTrue(memberInfo.HasAttribute<Attribute1>(inherit: true));
+		}
+
+		[Test]
+		public void HasAttributeTest()
+		{
+			var memberInfo = typeof(AttributeTestSubClass);
+			Assert.IsTrue(memberInfo.HasAttribute(typeof(Attribute2)));
+			Assert.IsFalse(memberInfo.HasAttribute(typeof(Attribute1), inherit: false));
+			Assert.IsTrue(memberInfo.HasAttribute(typeof(Attribute1), inherit: true));
+		}
 	}
 
 	class Attribute1 : Attribute
 	{
-
+		string Property1 { get; set; } = string.Empty;
 	}
 
 	class Attribute2 : Attribute
@@ -62,7 +82,14 @@ namespace Evans.Core.Extensions.System.Reflection.Tests
 
 	}
 
+	[AttributeUsage(validOn: AttributeTargets.Class, AllowMultiple = true)]
+	class AttributeMulti : Attribute
+	{
+		public string TextProperty { get; set; }
+	}
+
 	[Attribute1]
+	[AttributeMulti(TextProperty = "Test1")]
 	class AttributeTestClass
 	{
 		[Attribute2]
@@ -85,6 +112,8 @@ namespace Evans.Core.Extensions.System.Reflection.Tests
 	}
 
 	[Attribute2]
+	[AttributeMulti(TextProperty = "Test2")]
+	[AttributeMulti(TextProperty = "Test3")]
 	class AttributeTestSubClass : AttributeTestClass
 	{
 
